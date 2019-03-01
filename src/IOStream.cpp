d2d26ab9056a1bbf79ae261f31b9fa6ca7f3c193
Toliak <ibolosik@gmail.com>
2019-03-01 23:18:56 +0300

    Added main IOStream class

---
+++ src/IOStream.cpp
@@ -0,0 +1,14 @@
+#include "IOStream.h"
+
+namespace BeautifulStream
+{
+
+IOStream::IOStream(std::istream &istream)
+    : istream(&istream)
+{}
+
+IOStream::IOStream(std::istream &istream, std::ostream &ostream)
+    : istream(&istream), ostream(&ostream)
+{}
+
+}
--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,16 @@
+
+#include <iostream>
+#include <fstream>
+#include <memory>
+
+#include "IOStream.h"
+
 int main(int argc, char *argv[])
 {
+    BeautifulStream::IOStream console(std::cin, std::cout);
+
+    auto variable = console.input<int>("Enter int: ");
+    console.output("Got int: ", variable);
 
     return 0;
 }
