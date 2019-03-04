2a42f9b8ca17014670c3d5222d0b05d5b45faa6e
Toliak <ibolosik@gmail.com>
2019-03-04 13:21:51 +0300

    Prepared main.cpp

--- main.cpp
+++ main.cpp
@@ -22,16 +22,17 @@ int main(int argc, char *argv[])
 {
     BeautifulStream::IOStream console(std::cin, std::cout);
 
-    // auto variable = console.input<int>("Enter int: ");
-    //console.output("Got int: ", std::is_arithmetic<MyStruct>::value);
-
-    ///BeautifulStream::Table<std::initializer_list<int>> t({{1,2,3}});
-
     MyStruct ms{1, "beta"};
     std::string s0 = BeautifulStream::toString(ms);
     std::string s1 = BeautifulStream::toString(14);
     std::string s2 = BeautifulStream::toString(7.0);
     std::string s3 = BeautifulStream::toString("sampletext");
+
+    console.output(s0, "\n ", s1, '\n');
+    console.output(s2, "\n ", s3, '\n');
+
+    auto variable = console.input<int>("Enter int: ");
+    console.output("Got int: ", variable);
 
     return 0;
 }
