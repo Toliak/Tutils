c265807e120bfff1cc9f43c0e5f3e65befa8f90e
Toliak <ibolosik@gmail.com>
2019-03-07 11:01:23 +0300

    Created string concatenation speed checker test

--- tests.cpp
+++ tests.cpp
@@ -10,7 +10,7 @@ class toString: public ::testing::Test
 protected:
     void SetUp() override
     {
-        numbers = new long[5] {
+        numbers = new long[5]{
             999999999999999,
             150150150150,
             68574635,
@@ -18,7 +18,7 @@ protected:
             1,
         };
 
-        strings = new std::string[5] {
+        strings = new std::string[5]{
             "999999999999999",
             "150150150150",
             "68574635",
@@ -38,7 +38,6 @@ public:
     std::string *strings{};
 };
 
-
 TEST_F(toString, TestName)
 {
     EXPECT_EQ(BeautifulStream::toString(14), "14");
@@ -56,6 +55,63 @@ TEST_F(toString, VeryLagreNumberViaStd)
     for (size_t i = 0; i < 99999; i++) {
         EXPECT_EQ((BeautifulStream::__ToString<int, long>(numbers[i % 5]).value), strings[i % 5]);
     }
+}
+
+class StringMultiplier: public ::testing::Test
+{
+
+protected:
+    void TearDown() override
+    {
+
+    }
+    void SetUp() override
+    {
+
+    }
+
+public:
+    std::vector<std::string> stringVector{
+        "AAAAAA4444AAA",
+        "AAAABB4444CAA",
+        "AAAABBBAACCAA",
+        "AAAABBBAACCAA",
+        "AAAABBBADDAAA",
+        "AAAABBBADDAAA",
+    };
+    const size_t baseSize = stringVector[0].size();
+    const size_t times = 500000;
+    const size_t expectedSize = times * baseSize * stringVector.size();
+};
+
+TEST_F(StringMultiplier, EqualSize)
+{
+    for (const std::string &str : stringVector) {
+        EXPECT_EQ(str.size(), baseSize);
+    }
+}
+
+TEST_F(StringMultiplier, ByStringstream)
+{
+    std::ostringstream stream;
+    for (size_t i = 0; i < times; i++) {
+        for (const std::string &str : stringVector) {
+            stream << str;
+        }
+    }
+    std::string result = stream.str();
+    EXPECT_EQ(result.size(), expectedSize);
+}
+
+TEST_F(StringMultiplier, ByStringAppend)
+{
+    std::string result;
+    for (size_t i = 0; i < times; i++) {
+        for (const std::string &str : stringVector) {
+            result += str;
+        }
+    }
+    EXPECT_EQ(result.size(), expectedSize);
 }
 
 int main(int argc, char **argv)
